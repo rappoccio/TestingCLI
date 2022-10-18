fc3a3527c6fe46a328bb71cec7678a018be48106
rappoccio <rappoccio@gmail.com>
2022-10-18 18:29:25 -0400

    Adding file for unit test

---
+++ testing.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char ** argv)
+{
+  for ( unsigned int i = 0; i < argc; ++i ) {
+    std::cout << argv[i] << std::endl;
+  }
+  std::cout << "j : " << std::endl;
+  std::string j;
+  std::cin >> j; 
+  
+}
